06dcddbde5f07e7e52823f15684c7d1368e35356
Ishita Tayal <ishitatayal05@gmail.com>
2025-02-26 22:34:33 +0530

    .

--- app.py
+++ app.py
@@ -541,4 +541,5 @@ def all_quizzes():
 
 #finally run the program
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=7000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
